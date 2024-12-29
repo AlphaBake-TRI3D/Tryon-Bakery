ac6e672dbe4aefaa69adf57c7d3bdd590bdc44bf
Nitish Reddy Parvatham <nitish@Nitishs-MacBook-Pro.local>
2024-12-29 22:20:48 +0530

    0.1.9 release

--- tryon_tray/python/setup.py
+++ tryon_tray/python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="tryon-tray",
-    version="0.1.7",
+    version="0.1.9",
     author="Nitish",
     author_email="nitish@tri3d.in",
     description="A package for virtual try-on services integration",
