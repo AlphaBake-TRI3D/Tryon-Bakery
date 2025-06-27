44f5bdb8234d3519e9d7e9a6f09f5dac0734fab0
Ubuntu <ubuntu@ip-172-31-59-102.ec2.internal>
2025-06-27 11:15:38 +0000

    compare page

--- tryon_menu/main/views_comparision.py
+++ tryon_menu/main/views_comparision.py
@@ -3,8 +3,15 @@ import json
 
 def compare(request):
 
-    models_list = ['alphabake150-fast', 'alphabake150-quality', 'fashnai-quality']
-    comparision_data = json.load(open('samples/5pairs-v-small-comparision.json'))
+    # models_list = ['alphabake150-fast', 'alphabake150-quality', 'fashnai-quality']
+    comparision_data = json.load(open('samples/44_pairs_data-comparision.json'))
+
+    # get the model list from the comparision data
+    models_list = list(comparision_data[list(comparision_data.keys())[1]].keys())
+    print(models_list,comparision_data[list(comparision_data.keys())[1]])
+    models_list.remove('name')
+    models_list.remove('human')
+    models_list.remove('garment')
 
     comparisons = []
     for item_value in comparision_data.values():
